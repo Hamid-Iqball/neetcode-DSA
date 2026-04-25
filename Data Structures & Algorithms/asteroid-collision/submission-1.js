cb3e973819ce11a067296ccc7326dffc6b6979d0
Hamid Iqbal <150221115+Hamid-Iqball@users.noreply.github.com>
2026-04-25 22:48:00 +0500

    Add: asteroid-collision - submission-1

---
+++ Data Structures & Algorithms/asteroid-collision/submission-1.js
@@ -0,0 +1,29 @@
+class Solution {
+    /**
+     * @param {number[]} asteroids
+     * @return {number[]}
+     */
+    asteroidCollision(asteroids) {
+        let stack = []
+        for(let c of asteroids){
+            while(stack.length && c<0 && stack[stack.length-1]>0){
+                let diff = c + stack[stack.length-1]
+                if(diff < 0){
+                    stack.pop()
+                }else if(diff > 0){
+                    c = 0
+                }else{
+                    c =0
+                    stack.pop()
+                }
+            }
+
+
+            if(c!==0){
+                stack.push(c)
+            }
+        }
+
+        return stack
+    }
+}
