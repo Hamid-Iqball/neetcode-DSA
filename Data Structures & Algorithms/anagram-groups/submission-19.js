fe369de639b15c1bdc382f7f5c2299539d99fd74
Hamid Iqbal <150221115+Hamid-Iqball@users.noreply.github.com>
2026-04-27 23:04:04 +0500

    Add: anagram-groups - submission-19

---
+++ Data Structures & Algorithms/anagram-groups/submission-19.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs) {
+        let res = {}
+        for(let s of strs){
+            const codsrt = s.split('').sort().join("")
+
+            if(!res[codsrt]){
+                res[codsrt] = [s]
+            }else{
+
+            res[codsrt].push(s)
+            }
+            
+        }
+
+
+        return Object.values(res)
+    
+    }
+}
