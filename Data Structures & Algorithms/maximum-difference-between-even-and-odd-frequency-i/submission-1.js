a2554978f36be3cfc98229481c5211de0f2be0d1
Hamid Iqbal <150221115+Hamid-Iqball@users.noreply.github.com>
2026-05-01 12:29:47 +0500

    Add: maximum-difference-between-even-and-odd-frequency-i - submission-1

---
+++ Data Structures & Algorithms/maximum-difference-between-even-and-odd-frequency-i/submission-1.js
@@ -0,0 +1,29 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {number}
+     */
+    maxDifference(s) {
+        let count = new Array(26).fill(0)
+        for(let c of s){
+            count[c.charCodeAt(0)-'a'.charCodeAt(0)]++
+        }
+
+
+        let res  = -Infinity
+
+        for(const odd of count){
+            if(odd===0  || odd %2===0) continue
+
+            for(const even of count){
+                if(even===0 || even%2===1) continue
+                res = Math.max(res, odd-even)
+            }
+        }
+
+
+        return res
+
+        
+    }
+}
