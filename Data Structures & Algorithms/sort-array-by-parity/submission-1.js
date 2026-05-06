9470eb9268a2afb944497051d75e462a4c3a7406
Hamid Iqbal <150221115+Hamid-Iqball@users.noreply.github.com>
2026-05-06 18:11:47 +0500

    Add: sort-array-by-parity - submission-1

---
+++ Data Structures & Algorithms/sort-array-by-parity/submission-1.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    sortArrayByParity(nums) {
+        let i=0
+        let j = nums.length-1
+
+        while(i<=j){
+            if(nums[j]%2===0){
+                [nums[i], nums[j]] = [nums[j] , nums[i]]
+            }
+
+            if(nums[i]%2 ===0) i++
+            if(nums[j]%2 !==0) j--
+        }
+
+        return nums
+    }
+}
